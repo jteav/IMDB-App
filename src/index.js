fcf83059cb8982e67cee2703da6cc7bd60a2fe1e
jteav <jmteav@gmail.com>
2023-01-22 19:28:49 -0500

    Got search working

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const URL = 'https://www.omdbapi.com/?apikey=fd944f71';
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: '',
+            type: '',
+            results: []
+        };
 
         this.handleChange = this.handleChange.bind(this)
+        this.handleType = this.handleType.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
@@ -16,18 +23,46 @@ class Home extends React.Component {
         this.setState({value: event.target.value})
     }
 
+    handleType(event) {
+        this.setState({type: event.target.value})
+    }
+
     handleSubmit(event) {
-        console.log(this.state.value)
+        fetch(URL + "&s=" + this.state.value + "&type=" + this.state.type)
+        .then(res => res.json())
+        .then((result) => {
+            console.log('result', result.Search)
+            this.setState({results: result.Search})
+        })
+
         event.preventDefault()
     }
 
     render() {
         return (
             <div className="container mt-3">
+                <h1>IMDB App</h1>
                 <form onSubmit={this.handleSubmit} className="input-group">
                     <input type="text" className="form-control" placeholder="Search" value={this.state.value} onChange={this.handleChange}></input>
+
+                    <select value={this.state.type} onChange={this.handleType}>
+                        <option value="">All</option>
+                        <option value="movie">Movie</option>
+                        <option value="series">Series</option>
+                        <option value="game">Game</option>
+                    </select>
+
                     <input type="submit" className="btn btn-primary" value="Search"></input>
                 </form>
+
+                <ul className="list-group">
+                    {this.state.results.map(result => (
+                        <li key={result.imdbID} className="list-group-item">
+                            {result.Title}
+                            <img src={result.Poster} className="img-fluid" alt='Poster'></img>
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
